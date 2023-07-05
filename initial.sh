c8fcff7ea64daacbffdc15b993d1243343c634c0
Joao <kandrocmatunein@gmail.com>
2023-07-05 13:32:25 -0300

    links

---
+++ initial.sh
@@ -0,0 +1,19 @@
+
+##########
+#        #
+# Assets #
+#        #
+##########
+
+# GRATIS:
+# https://materialfuture.itch.io/gameboy-assets
+
+# PAGOS:
+# https://fizzlymike.itch.io/r
+
+# IDEIAS:
+# https://sleepingpandagames.itch.io/gameboy-like-color-transition-template-for-construct-3
+# https://tilingo.itch.io/shining-fields
+# https://tilingo.itch.io/shining-fields-dazzling-wilderness-16x16-asset-pack
+# https://tilingo.itch.io/shining-fields-sparkling-town-16x16-asset-pack
+# https://tilingo.itch.io/shining-fields-ghostly-autumn-16x16-asset-pack
